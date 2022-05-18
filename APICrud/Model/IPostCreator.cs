d70167f2c3fa737cfb1e4d4b12a2369e96071b81
Pouchain Emilie <emilie.pouchain@viacesi.fr>
2022-05-18 11:00:05 +0200

    Creation du post creator avec le CRUD

--- APICrud/Model/IPostCreator.cs
+++ APICrud/Model/IPostCreator.cs
@@ -8,5 +8,6 @@ namespace APICrud.Model
 {
     public interface IPostCreator
     {
+
     }
 }
--- APICrud/Model/PostCreator.cs
+++ APICrud/Model/PostCreator.cs
@@ -1,18 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace APICrud.Model 
 {
+    public class Post
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+    }
     public class PostCreator :  IPostCreator
     {
-        public PostCreator()
-        {
+        static HttpClient client= new HttpClient();
 
+        static async Task<Uri> CreatePostAsync(Post post)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/posts", post);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
         }
 
+        static async Task<Post> GetPostAsync(string path)
+        {
+            Post post = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                post = await response.Content.ReadAsAsync<Post>();
+            }
+            return post;
+        }
 
+        static async Task<Post> UpdatePostAsync(Post post)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/posts/{post.id}", post);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            post = await response.Content.ReadAsAsync<Post>();
+            return post;
+        }
+
+        static async Task<HttpStatusCode> DeletePosttAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"api/posts/{id}");
+            return response.StatusCode;
+        }
     }
 }
